ce72d53144f676bb838f2db74c3f4b271987042a
Suraj Kumar <surajkumarok20@gmail.com>
2024-05-27 23:29:00 +0530

    sorting techinique

--- searchInSortedMtrx.cpp
+++ searchInSortedMtrx.cpp
@@ -23,6 +23,29 @@ void bruteSearch(int mtrx[][4], int n, int key){
     }
 }
 
+
+
+bool search(int mtrx[][4], int n, int m, int key){
+    int i = 0, j = m-1;
+
+    while(i < n && j >= 0){
+        if(mtrx[i][j] == key){
+            cout<< " found at cell "<< i <<","<<j<<endl;
+            return true;
+        }
+        else if(mtrx[i][j] > key){
+            // left
+            j--;
+        }
+        else{
+         //   down
+            i++;
+        }
+    }
+    cout<<"key not found"<<endl;
+    return false;
+}
+
 int main()
 {
     int mtrx[4][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
